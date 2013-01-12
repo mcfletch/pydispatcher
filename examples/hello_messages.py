44e57bc7efac42a8c4e702d6b06b18ebc8fb4c1a
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-01-12 15:26:35 -0500

    More print function changes

--- examples/hello_messages.py
+++ examples/hello_messages.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
+from __future__ import print_function
 from pydispatch import dispatcher
 SIGNAL = 'my-first-signal'
 
 def handle_event( sender ):
     """Simple event handler"""
-    print 'Signal was sent by', sender
+    print('Signal was sent by', sender)
 dispatcher.connect( handle_event, signal=SIGNAL, sender=dispatcher.Any )
 
 first_sender = object()
