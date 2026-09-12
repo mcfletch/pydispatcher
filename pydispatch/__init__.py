ab297afd8d9ecd2836f88acd84e4d02f2a106d11
Mike C. Fletcher <mcfletch@vrplumber.com>
2026-09-12 00:35:18 -0400

    VERSION BUMP We are an alpha with the introduction of the index

--- pydispatch/__init__.py
+++ pydispatch/__init__.py
@@ -1,6 +1,6 @@
 """Multi-consumer multi-producer dispatching mechanism
 """
-__version__ = "2.0.8"
+__version__ = "2.0.9a1"
 __author__ = "Patrick K. O'Brien"
 __maintainer__ = "Mike C. Fletcher"
 __license__ = "BSD"
